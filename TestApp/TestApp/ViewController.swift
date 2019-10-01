a3d9a835d18cddd6a3ee4be17bba52f3d41c82e9
y-okudera <appledev.yuoku@gmail.com>
2019-10-01 20:36:26 +0900

    新規プロジェクトを作成

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Yuki Okudera on 2019/10/01.
+//  Copyright © 2019 Yuki Okudera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
